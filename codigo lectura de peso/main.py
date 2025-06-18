27d8cbec9117eae87f28f583d84a86d233aa5b7f
J Leonardo Valadez <juan.valadezo@uanl.edu.mx>
2025-06-17 22:53:10 -0600

    summary archives
    the code main.py was changed so it can read in differential channel

--- codigo lectura de peso/main.py
+++ codigo lectura de peso/main.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-
 import time
 import ADS1256
 import RPi.GPIO as GPIO
 
-
 try:
+    # Inicializa el ADC
     ADC = ADS1256.ADS1256()
     ADC.ADS1256_init()
 
-    while(1):
-        ADC_Value = ADC.ADS1256_GetAll()
-        print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff))
-        print ("1 ADC = %lf"%(ADC_Value[1]*5.0/0x7fffff))
-        print ("2 ADC = %lf"%(ADC_Value[2]*5.0/0x7fffff))
-        print ("3 ADC = %lf"%(ADC_Value[3]*5.0/0x7fffff))
-        print ("4 ADC = %lf"%(ADC_Value[4]*5.0/0x7fffff))
-        print ("5 ADC = %lf"%(ADC_Value[5]*5.0/0x7fffff))
-        print ("6 ADC = %lf"%(ADC_Value[6]*5.0/0x7fffff))
-        print ("7 ADC = %lf"%(ADC_Value[7]*5.0/0x7fffff))
-        print ("\33[9A")
+    # Configura la ganancia del amplificador interno (64x)
+    ADC.ADS1256_SetGain(ADC.GAIN_64)
 
-        
-except :
+    print("Inicio de lectura de celda de carga (AIN0 - AIN1)...\n")
+
+    while True:
+        # Selecciona lectura diferencial entre AIN0 y AIN1 (canal 0)
+        ADC.ADS1256_SetDiffChannal(0)
+        raw_value = ADC.ADS1256_GetChannalValue(0)
+
+        # Convierte a voltaje
+        voltage = raw_value * 5.0 / 0x7FFFFF  # 24 bits
+        print(f"Voltaje diferencial: {voltage:.6f} V")
+
+        time.sleep(0.2)
+
+except KeyboardInterrupt:
     GPIO.cleanup()
-    print ("\r\nProgram end     ")
+    print("\nLectura interrumpida por el usuario.")
+
+except Exception as e:
+    GPIO.cleanup()
+    print(f"\nError: {e}")
     exit()
